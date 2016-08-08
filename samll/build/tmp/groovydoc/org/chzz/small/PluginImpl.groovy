ffdbb35d7d48ebac947925fcf7269eb21f00884a
copy <xiaoxinxing12@qq.com>
2016-08-08 19:31:32 +0800

    Initial commit

---
+++ samll/build/tmp/groovydoc/org/chzz/small/PluginImpl.groovy
@@ -0,0 +1,10 @@
+package org.chzz.small
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl implements Plugin<Project> {
+    void apply(Project project) {
+        project.gradle.addListener(new TimeListener())
+    }
+}
